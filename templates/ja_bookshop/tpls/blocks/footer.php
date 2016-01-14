98677e77fb7aa2adf170711a15cd12a463da978a
PhuND <phund@paraline.com.vn>
2016-01-14 17:38:23 +0700

    Fix footer

--- templates/ja_bookshop/tpls/blocks/footer.php
+++ templates/ja_bookshop/tpls/blocks/footer.php
@@ -19,23 +19,38 @@ defined('_JEXEC') or die;
 		<!-- FOOT NAVIGATION -->
 		<div class="container">
 			<div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 footer-info">
-          <?php if ($this->countModules('footer-info')) : ?>
-              <jdoc:include type="modules" name="<?php $this->_p('footer-info') ?>" style="T3xhtml" />
-          <?php endif ?>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 footer-links">
-          
-            <?php if ($this->countModules('footer-subcribe')) : ?>
-              <jdoc:include type="modules" name="<?php $this->_p('footer-subcribe') ?>" style="T3xhtml" />
-            <?php endif ?>
-          
-            <?php if ($this->checkSpotlight('footnav', 'footer-1, footer-2, footer-3, footer-4, footer-5, footer-6')) : ?>
-              <?php $this->spotlight('footnav', 'footer-1, footer-2, footer-3, footer-4, footer-5, footer-6') ?>
-            <?php endif ?>
-          
-        </div>
-      </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 footer-info">
+                    <?php if ($this->countModules('footer-info')) : ?>
+                        <jdoc:include type="modules" name="<?php $this->_p('footer-info') ?>" style="T3xhtml" />
+                    <?php endif ?>
+                </div>
+
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 footer-links">
+                    <div class=" col-lg-3 col-md-3  col-sm-3 hidden-sm  col-xs-12">
+                                        <?php if ($this->countModules('footer-logo')) : ?>
+                                            <jdoc:include type="modules" name="<?php $this->_p('footer-logo') ?>" style="T3xhtml" />
+                                        <?php endif ?>
+                    </div>
+                    <div class=" col-lg-6 col-md-6  col-sm-6 hidden-sm  col-xs-12">
+                    <?php if ($this->countModules('footer-subcribe')) : ?>
+                        <jdoc:include type="modules" name="<?php $this->_p('footer-subcribe') ?>" style="T3xhtml" />
+                    <?php endif ?>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 footer-links">
+                    <div class=" col-lg-2 col-md-2  col-sm-2 hidden-sm  col-xs-12"></div>
+                    <div class=" col-lg-3 col-md-3  col-sm-3 hidden-sm  col-xs-12">
+                        <?php if ($this->countModules('footer-1')) : ?>
+                            <jdoc:include type="modules" name="<?php $this->_p('footer-1') ?>" style="T3xhtml" />
+                        <?php endif ?>
+                    </div>
+                    <div class=" col-lg-4 col-md-4  col-sm-4 hidden-sm  col-xs-12">
+                        <?php if ($this->countModules('footer-2')) : ?>
+                            <jdoc:include type="modules" name="<?php $this->_p('footer-2') ?>" style="T3xhtml" />
+                        <?php endif ?>
+                    </div>
+                </div>
+            </div>
 		</div>
 		<!-- //FOOT NAVIGATION
 
